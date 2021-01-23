4ba3a3e437b86c697168686dc32162c17f1bcfd9
Dexter <dex429.dc@gmail.com>
2021-01-23 01:16:08 -0500

    commit tree 9255dee0f24d163dd2d0974b1b9907b838998868

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,14 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+       person.partner = self
+  end
+
+  class PartnerError < StandardError
+
   end
 
 end
@@ -15,6 +22,7 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
+
 
 
 
